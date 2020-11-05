e5ee95bf135977b16e03a28b1b2f066d3d4e12e4
yui421 <banhgao.nya@gmail.com>
2020-11-05 21:01:14 +0700

    fix: endpoint

--- src/services/tichhop247.ts
+++ src/services/tichhop247.ts
@@ -4,7 +4,8 @@ import qs from 'qs'
 export default class Tichhop247 implements ITichhop247 {
   private _apikey: string;
 
-  private _ENDPOINT = 'http://tichhop247.com/API/NapThe'
+  private _SEND_ENDPOINT = 'http://tichhop247.com/API/NapThe'
+  private _CHECK_ENDPOINT = 'http://tichhop247.com/API/TrangThai'
   private _CALLBACK_URL = 'https://google.com'
 
   constructor(apikey: string) {
@@ -12,15 +13,15 @@ export default class Tichhop247 implements ITichhop247 {
     this._apikey = apikey
   }
 
-  private sendGET(data: any): Promise<any> {
+  private sendGET(type: 'SEND' | 'CHECK', data: any): Promise<any> {
     const headers: request.Headers = {
       'Content-Type': 'application/json' 
     }
 
     const otps: request.OptionsWithUri = {
-      uri: this._ENDPOINT,
+      uri: (type === 'SEND') ? this._SEND_ENDPOINT : this._CHECK_ENDPOINT,
       method: 'GET',
-      body: qs.stringify(Object.assign({ APIKey: this._apikey }, data)),
+      qs: Object.assign({ APIKey: this._apikey }, data),
       json: true
     }
 
@@ -47,7 +48,7 @@ export default class Tichhop247 implements ITichhop247 {
       Code: '1. Là hệ thống đã nhận thẻ' | '0. Lỗi, hệ thống từ chối thẻ này ' 
     }
 
-    const resp: _IResponse = await this.sendGET({
+    const resp: _IResponse = await this.sendGET('SEND',{
       Network: network,
       CardCode: privateCode,
       CardSeri: series,
@@ -56,17 +57,14 @@ export default class Tichhop247 implements ITichhop247 {
       TrxID: transactionID,
     })
 
-    console.log(resp)
-
     return resp.Code === '1. Là hệ thống đã nhận thẻ'
   }
 
   public async CheckStatus(transactionID: string): Promise<ICardCheckStatus> {
-    const resp: ICardCheckStatus = await this.sendGET({
+    const resp: ICardCheckStatus = await this.sendGET('CHECK', {
       TrxID: transactionID,
     })
 
-    console.log(resp)
     return resp
   }
 
--- src/test/tichhop247.ts
+++ src/test/tichhop247.ts
@@ -1,6 +1,12 @@
 import Tichhop247 from '../services/tichhop247'
-import config from 'config'
+import config from '../config'
 
 const tichhop247 = new Tichhop247(config.tichhop247_key || '')
 
-tichhop247.Add()
+tichhop247
+  .Add('VNP', '31287547201982', '59000013736344', 10000, '123478ds10')
+  .then(console.log)
+
+tichhop247
+  .CheckStatus('123478ds10')
+  .then(console.log)
