45f19c90af5471c76119792cf783657db2026435
yui421 <banhgao.nya@gmail.com>
2020-11-18 00:15:34 +0700

    new method: findByID (user)

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,14 +5,28 @@ export default class UserModel implements Models.User {
   public addUser(user: IUser): Promise<any> {
     return new Promise((resolve, reject) => {
       databaseConnect(connection => {
-        connection.query("INSERT INTO `USER` (`ID`, `name`, `avatar`, `roler`) VALUES (?, ?, ?, ?); ", 
-          [ null, user.name, user.avatar, user.role ]
+        connection.query("INSERT INTO `USER` (`ID`, `user_id`, `name`, `avatar`, `roler`) VALUES (?, ?, ?, ?); ", 
+          [ null, user.user_id, user.name, user.avatar, user.role ]
         ,(err, results, fields) => {
           if (err) throw err
           
           resolve(results)
           connection.end()
         })
+      })
+    })
+  }
+
+  public findByID(userID: number): Promise<IUser> {
+    return new Promise((resolve, reject) => {
+      databaseConnect(connection => {
+        connection.query("SELECT * FROM `USER` WHERE `USER`.`user_id` = ?", [userID], 
+          (err, results, fields) => {
+            if (err) throw err
+
+            resolve(results)
+            connection.end()
+          })
       })
     })
   }
