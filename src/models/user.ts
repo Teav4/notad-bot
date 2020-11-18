958126257e455cd2d7fb7cd62f63e1c97222c942
yui421 <banhgao.nya@gmail.com>
2020-11-18 23:48:27 +0700

    add: findByFacebookPSID method

--- src/models/user.ts
+++ src/models/user.ts
@@ -5,8 +5,8 @@ export default class UserModel implements Models.User {
   public addUser(user: IUser): Promise<any> {
     return new Promise((resolve, reject) => {
       databaseConnect(connection => {
-        connection.query("INSERT INTO `USER` (`ID`, `user_id`, `name`, `avatar`, `roler`) VALUES (?, ?, ?, ?); ", 
-          [ null, user.user_id, user.name, user.avatar, user.role ]
+        connection.query("INSERT INTO `USER` (`ID`, `user_id`, `facebook_psid`, `name`, `avatar`, `roler`) VALUES (?, ?, ?, ?, ?, ?); ", 
+          [ null, user.user_id, user.facebook_psid, user.name, user.avatar, user.role ]
         ,(err, results, fields) => {
           if (err) throw err
           
@@ -17,20 +17,33 @@ export default class UserModel implements Models.User {
     })
   }
 
-  public findByID(userID: number): Promise<IUser> {
+  public findByID(userID: number): Promise<IUser|null> {
     return new Promise((resolve, reject) => {
       databaseConnect(connection => {
-        connection.query("SELECT * FROM `USER` WHERE `USER`.`user_id` = ?", [userID], 
+        connection.query("SELECT * FROM `USER` WHERE `USER`.`user_id` = ?", [userID],
           (err, results, fields) => {
             if (err) throw err
 
-            resolve(results)
+            resolve(results[0] ? results[0] : null)
             connection.end()
           })
       })
     })
   }
 
+  public findByFacebookPSID(facebookPSID: string): Promise<IUser|null> {
+    return new Promise((resolve, reject) => {
+      databaseConnect(connection => {
+        connection.query("SELECT * FROM `USER` WHERE `USER`.`facebook_psid` = ?", [facebookPSID],
+          (err, results, fields) => {
+            if (err) throw err
+
+            resolve(results[0] ? results[0] : null)
+            connection.end()
+          })
+      })
+    })
+  }
   public removeUser() {
 
   }
