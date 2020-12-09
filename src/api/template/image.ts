f080fc58d8a9047267f0ab06666110a8de2e76bb
yui421 <teav4@feadorra.com>
2020-12-10 03:11:19 +0700

    create image message #1

---
+++ src/api/template/image.ts
@@ -0,0 +1,29 @@
+import MessengerAPI from '../../services/messengerAPI'
+
+const messengerAPI = new MessengerAPI()
+
+export default async function createImageMessage(imageUrl: string|string[]) {
+  
+  const _imageUrl = (typeof imageUrl === 'string') ? [ imageUrl ] : imageUrl
+  let imageUploadResponse: Array<Messenger.API.uploadMediaResponse|null> = await Promise.all(_imageUrl.map(url => messengerAPI.uploadMediaFile(url)))
+
+  let elements: any = []
+
+  imageUploadResponse.forEach(image => {
+    if (image !== null) {
+      elements.push({
+        "media_type": "<image>",
+        "attachment_id": image.attachment_id
+      })
+    }
+  })
+
+  return {
+    type: "template",
+    payload: {
+       template_type: "media",
+       elements,
+    }
+  }
+
+}
